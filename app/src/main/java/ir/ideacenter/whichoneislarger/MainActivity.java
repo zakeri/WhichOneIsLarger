5e5a94555e3307331919b05de66f5be082887665
Hassan Zakeri <h.zakeri@gmail.com>
2018-09-11 19:24:56 +0430

    Adding start button and a ConstraintLayout

--- app/src/main/java/ir/ideacenter/whichoneislarger/MainActivity.java
+++ app/src/main/java/ir/ideacenter/whichoneislarger/MainActivity.java
@@ -2,19 +2,37 @@ package ir.ideacenter.whichoneislarger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GameFragment gameFragment = new GameFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_game_container, gameFragment)
-                .addToBackStack(null)
-                .commit();
+        findViews();
+        configure();
+    }
+
+    private void findViews() {
+        startGame = (Button) findViewById(R.id.start_game);
+    }
+
+    private void configure() {
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GameFragment gameFragment = new GameFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_game_container, gameFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
